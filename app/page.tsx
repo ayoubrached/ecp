226ed2ff2e848d1f8d9ad2ac38c2ef47daf08a4d
Ayoub Rached <ayoubrached10@gmail.com>
2025-11-06 16:38:03 -0500

    Add minimal Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,11 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 style={{ margin: 0, marginBottom: 8 }}>ECP Utilities</h1>
+      <p style={{ color: '#374151' }}>
+        Use the navigation above to access tools.
+      </p>
+    </main>
+  );
+}
 
